331a17ae6c1b1e9fbd5bdbdc7a29b7968e359a3d
Ugur Can <ugurcnakkoc@gmail.com>
2024-09-09 14:09:15 +0300

    Added bottom navigation

--- app/src/main/java/com/example/lingualab/MainActivity.kt
+++ app/src/main/java/com/example/lingualab/MainActivity.kt
@@ -5,19 +5,34 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.lingualab.databinding.ActivityMainBinding
 import dagger.hilt.EntryPoint
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        createBottomNavigation()
+    }
+
+
+    private fun createBottomNavigation(){
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val bottomNavigation = binding.bottomNavigation
+        bottomNavigation.setupWithNavController(navHostFragment.navController)
+
     }
 }
